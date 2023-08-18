5c70004f7d916258c0e7efe45736c63b809bab9e
abdulraffay123 <abdulraffay252@gmail.com>
2023-08-19 03:55:12 +0500

    ToDo With React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  const addItem = () => {
+    if (newItem.trim() !== '') {
+      setItems([...items, newItem]);
+      setNewItem('');
+    }
+  };
+
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((_, i) => i !== index);
+    setItems(updatedItems);
+  };
+
+  const editItem = (index, newValue) => {
+    const updatedItems = [...items];
+    updatedItems[index] = newValue;
+    setItems(updatedItems);
+  };
+
+  const deleteAllItems = () => {
+    setItems([]);
+  };
+
   return (
     <div className="App">
-      <nav className="navbar">
-        <div className="navbar-brand">DORSIN</div>
-        <ul className="navbar-nav">
-          <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
-          <li className="nav-item"><a href="/services" className="nav-link">Services</a></li>
-          <li className="nav-item"><a href="/Features" className="nav-link">Features</a></li>
-          <li className="nav-item"><a href="/contact" className="nav-link">Pricing</a></li>
-          <li className="nav-item"><a href="/contact" className="nav-link">Team</a></li>
-          <li className="nav-item"><a href="/contact" className="nav-link">Blog</a></li>
-          <li className="nav-item"><a href="/contact" className="nav-link">Contact</a></li>
-        </ul>
-      </nav>
-      <div className="background-image"> </div>
-
+      <h1>ToDo App</h1>
+      <div>
+        <input
+          type="text"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button onClick={addItem}>Add</button>
+      </div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => editItem(index, prompt('Enter new value:'))}>Edit</button>
+            <button onClick={() => deleteItem(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={deleteAllItems}>Delete All</button>
     </div>
-
   );
-
-
 }
 
+
 export default App;
+
+
